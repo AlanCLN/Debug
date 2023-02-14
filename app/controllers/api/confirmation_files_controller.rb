c5992b2da25977717ed2f98fb6c8878899907e31
alancln <alan.ng8912@gmail.com>
2023-02-13 20:24:25 -0500

    sample code

--- app/controllers/api/confirmation_files_controller.rb
+++ app/controllers/api/confirmation_files_controller.rb
@@ -6,30 +6,39 @@ require_relative '../../scripts/generate_packing_slip'
 class Api::ConfirmationFilesController < ApplicationController
 
   def create
-    
+    if !params['0'] || !params['1']
+      render json: ['Please submit exactly two files by highlighting both files with CTRL or SHIFT'], status: 422
+      return
+    end
     if params['2']
       render json: ['Only two files can be uploaded'], status: 422
       return
     end
 
-    orders = get_orders_from_params(params)
-
-    replacements = {}
-    Replacement.all.each{|replacement| replacements[replacement[:find]] = replacement[:replace]}
-    replace_product_name!(orders,replacements)
-
+    replacements = Replacement.all
+    orders_and_items_info_hash = get_orders_from_params(params, replacements)
     
+    if !orders_and_items_info_hash
+      render json: ['A "new file" was not submitted. Please check to see if the new file (larger file) has a "item-price" header'], status: 422
+      return
+    end
+
+    unshipped_orders_info, new_order_items_info = orders_and_items_info_hash
+
     tracked_price = Setting.find_by(setting: 'tracking')[:value]
-    # orders = remove_tracked_orders(orders,tracked_price)
+    tracked_packing_slips, untracked_packing_slips = generate_packing_slips(unshipped_orders_info, new_order_items_info, tracked_price)
 
-    cards = {}
-    Card.all.each{|card| cards[card[:name]] = card[:category]}
-    
+    # generate_confirmation_file should return an array of order ids to the frontend
+    # generate_pull_sheet should return a hashmap of all cards with quantity and sorted by card name
+    # tracked and untracked packing slips are hashmaps with all the information for each order
+
     files_for_frontend = {
-      'confirmation_file'=> orders,
-      'pullsheet'=> generate_pull_sheet(orders,cards),
-      'packing_slips'=> generate_packing_slip(orders)
+      'confirmation_file'=> generate_confirmation_file(unshipped_orders_info),
+      'pullsheet'=> generate_pull_sheet(unshipped_orders_info, new_order_items_info),
+      'tracked_packing_slips'=> tracked_packing_slips,
+      'untracked_packing_slips'=> untracked_packing_slips
     }
+
     if files_for_frontend
       render json: files_for_frontend
     else
--- app/scripts/file_script_utils.rb
+++ app/scripts/file_script_utils.rb
@@ -1,104 +1,176 @@
 
+# representation of how the orders hashmap look like:
 
-# orders = {
-#   order_id: {
-      # order-id
-      # order-item-id
-      # purchase-date
-      # payments-date
-      # reporting-date
-      # promise-date
-      # days-past-promise
-      # buyer-email
-      # buyer-name
-      # buyer-phone-number
-      # sku
-      # number-of-items
-      # product-name
-      # quantity-purchased
-      # quantity-shipped
-      # quantity-to-ship
-      # ship-service-level
-      # recipient-name
-      # ship-address-1
-      # ship-address-2
-      # ship-address-3
-      # ship-city
-      # ship-state
-      # ship-postal-code
-      # ship-country
-      # is-business-order
-      # purchase-order-number
-      # price-designation
+# unshipped_orders_info = {
+#   order-id: {
+#     products: [order-items-id],  # an array of order-item-id
+#     recipient-name,
+#     ship-address-1,
+#     ship-address-2,
+#     ship-addres-3,
+#     ship-city,
+#     ship-state,
+#     ship-postal-code,
+#     ship-country,
+#     total-price-without-tax,
 #   }
 # }
+
+# new_order_items_info = {
+#   order-item-id: {
+#     product-name,
+#     quantity-purchased,
+#     item-price,
+#     item-tax,
+#   }
+# }
+REPLACEMENTS = []
+
+def get_orders_from_params(params, replacements)
+
+  # An active record replacement object is passed in as an argument. We woud like to extract
+  # all the replacement strings and sort them based on longest to shortest. Then set it as a
+  # CONSTANT above.
+  extract_and_sort_replacements(replacements)
+
+  # file1 and file2 will each return an array.
+  # Each element in the array is a string representing each row of the file
+  # The first element (index 0) represents the headers divided by an escaped tab char -> \t
+  file1 = params['0'].open.readlines.map(&:chomp)
+  file2 = params['1'].open.readlines.map(&:chomp)
+
+  # categorize the files by figuring out which file is unshipped and which file is new
+  files = categorize_files(file1, file2)
+  # In our categorize_files function, we return [false, false] if both files are not the new_file
+  # we return false so our controller can render an error that we did not submit a new_file
+  if !files
+    return false
+  end
+  unshipped_file, new_file = files
+
+  new_order_items_info = turn_new_file_into_hash(new_file)
+  unshipped_orders_info = turn_unshipped_file_into_hash(unshipped_file, new_order_items_info)
+
+  return [unshipped_orders_info, new_order_items_info]
+end
+
+def extract_and_sort_replacements(replacements)
+
+  replacements.each do |replacement|
+    REPLACEMENTS << replacement[:prefix]
+  end
+
+  REPLACEMENTS.sort_by!{|prefix| prefix.length}.reverse!
+
+end
+
+def categorize_files(file1, file2)
+  # we're passing in the 0th index because the headers are located in the 0th index
+  # the headers will tell us which one is new_file because new_file has item-price and 
+  # unshipped doesn't
+  file1_headers = parse_line_into_fields(file1[0])
+  file2_headers = parse_line_into_fields(file2[0])
+
+  if file1_headers.include?('item-price')
+    new_file = file1
+    unshipped_file = file2
+  elsif file2_headers.include?('item-price')
+    new_file = file2
+    unshipped_file = file1
+  else
+    return false
+  end
+
+  return [unshipped_file, new_file]
+end
 
 def parse_line_into_fields(line)
   fields = line.split(/\t/)
   return fields
 end
 
-def turn_fields_into_hash(headers,fields)
-  order = {}
-  headers.zip(fields).each do |header,field|
-    order[header] = field
-  end
-  return order
-end
+def turn_new_file_into_hash(new_file)
+  headers = parse_line_into_fields(new_file[0])
+  new_order_items_info = Hash.new{ |h,k| h[k] = Hash.new }
 
-def turn_file_into_order_hash(file_lines)
-  headers = parse_line_into_fields(file_lines[0])
-  order_hash = {}
-  file_lines[1...file_lines.length].each do |line|
+  new_file[1...new_file.length].each do |line|
     fields = parse_line_into_fields(line)
-    order = turn_fields_into_hash(headers,fields)
-    order_hash[order['order-id']] = order
+    info_hash = turn_fields_into_hash(headers, fields)
+
+    order_item_id = info_hash['order-item-id']
+    product_name = strip_product_name_line(info_hash['product-name'])
+    quantity_purchased = info_hash['quantity-purchased']
+    item_price = info_hash['item-price']
+    item_tax = info_hash['item-tax']
+
+    new_order_items_info[order_item_id]['product_name'] = product_name
+    new_order_items_info[order_item_id]['quantity_purchased'] = quantity_purchased
+    new_order_items_info[order_item_id]['item_price'] = item_price
+    new_order_items_info[order_item_id]['item_tax'] = item_tax
   end
-  return order_hash
+
+  return new_order_items_info
 end
 
-def replace_product_name!(orders,replacements)
-  orders.each do |id,order|
-    replacements.each do |find,replace|
-      order['product-name'].sub!(find,replace)
+def strip_product_name_line(product_name)
+
+  REPLACEMENTS.each do |replacement|
+    if product_name.start_with?(replacement)
+      replacement_length = replacement.length
+      return product_name[replacement_length..-1]
     end
   end
+
+  return product_name
 end
 
-def remove_tracked_orders(orders,tracked_price)
-  # filtered_orders = {}
-  # orders.each do |id,order|
-  #   if order['value'] < tracked_price
-  #     filtered_orders[id] = order
-  #   end
-  # end
-  # return filtered_orders
-  orders.select! {|id,order| order['value']<tracked_price}
+def turn_unshipped_file_into_hash(unshipped_file, new_order_items_info)
+  headers = parse_line_into_fields(unshipped_file[0])
+  unshipped_orders_info = Hash.new{ |h,k| h[k] = Hash.new(0) }
+
+  unshipped_file[1...unshipped_file.length].each do |line|
+    fields = parse_line_into_fields(line)
+    info_hash = turn_fields_into_hash(headers, fields)
+
+    order_id = info_hash['order-id']
+    order_item_id = info_hash['order-item-id']
+    recipient_name = info_hash['recipient-name']
+    ship_address_1 = info_hash['ship-address-1']
+    ship_address_2 = info_hash['ship-address-2']
+    ship_address_3 = info_hash['ship-address-3']
+    ship_city = info_hash['ship-city']
+    ship_state = info_hash['ship-state']
+    ship_postal_code = info_hash['ship-postal-code']
+    ship_country = info_hash['ship-country']
+
+    # grab the item_price from new_order_items_info and turn the string into a float
+    item_price = new_order_items_info[order_item_id]['item_price'].to_f
+
+    # initialize a new array for the products key if the hash for the order_id does not have a products key
+    if !unshipped_orders_info.has_key?(order_id) || !unshipped_orders_info[order_id].has_key?('products')
+      unshipped_orders_info[order_id]['products'] = []
+    end
+
+    unshipped_orders_info[order_id]['products'] << order_item_id
+    unshipped_orders_info[order_id]['recipient_name'] = recipient_name
+    unshipped_orders_info[order_id]['ship_address_1'] = ship_address_1
+    unshipped_orders_info[order_id]['ship_address_2'] = ship_address_2
+    unshipped_orders_info[order_id]['ship_address_3'] = ship_address_3
+    unshipped_orders_info[order_id]['ship_city'] = ship_city
+    unshipped_orders_info[order_id]['ship_state'] = ship_state
+    unshipped_orders_info[order_id]['ship_postal_code'] = ship_postal_code
+    unshipped_orders_info[order_id]['ship_country'] = ship_country
+    unshipped_orders_info[order_id]['total_price_without_tax'] += item_price
+  end
+  return unshipped_orders_info
 end
 
-def add_order_price_to_orders(file1,file2)
-  if file1.values[0].has_key?('item-price')
-    new_order = file1
-    unshipped_order = file2
-  else
-    new_order = file2
-    unshipped_order = file1
+def turn_fields_into_hash(headers,fields)
+
+  info_hash = {}
+  headers.zip(fields).each do |header,field|
+    info_hash[header] = field
   end
 
-  unshipped_order.each do |order_id,order|
-    order_price = new_order[order_id]['item-price'].to_i
-    unshipped_order[order_id]['item-price'] = order_price
-  end
-
-  return unshipped_order
-end
-
-def get_orders_from_params(params)
-  file1 = params['0'].open.readlines.map(&:chomp)
-  file2 = params['1'].open.readlines.map(&:chomp)
-
-  file1_hash = turn_file_into_order_hash(file1)
-  file2_hash = turn_file_into_order_hash(file2)
-
-  return add_order_price_to_orders(file1_hash,file2_hash)
+  return info_hash
 end
